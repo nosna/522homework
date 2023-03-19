f835004b3fdfd1094f906a4190464105a2372f8e
nosna <ansonhuang1216@gmail.com>
2023-03-18 20:05:27 -0400

    hack

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -13,4 +13,9 @@ class CONFIG:
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=0.006)
 
-    transforms = Compose([ToTensor(), Normalize(mean=[0.4914, 0.4822, 0.4465],std=[0.2023, 0.1994, 0.2010])])
+    transforms = Compose(
+        [
+            ToTensor(),
+            Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
+        ]
+    )
